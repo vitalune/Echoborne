c1e80dc38a65573d6501ba23a6f95b7ac6328fa1
Amir Valizadeh <amirv2021@gmail.com>
2025-05-17 22:14:03 -0400

    add base movement/targeting mechanic for enemy

---
+++ Assets/Scripts/LightBanditMovement.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class LightBanditMovement : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5f;
+    public float jumpArcHeight = 2f; // Desired jump arc height
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool shouldJump;
+    private Vector2 jumpTarget;
+    private Vector2 calculatedJumpVelocity;
+
+    // Checks if the bandit is on the ground
+    private bool GroundCheck()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = GroundCheck();
+        float direction = Mathf.Sign(player.position.x - transform.position.x);
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+
+            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
+            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 2f, groundLayer);
+            RaycastHit2D platformAhead = Physics2D.Raycast(transform.position + new Vector3(direction * 2f, 0, 0), Vector2.down, 5f, groundLayer);
+            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, groundLayer);
+
+            // Smart jump: if there's a gap or obstacle, calculate jump to next platform
+            if ((!groundInFront.collider && !gapAhead.collider) || (groundInFront.collider && !groundInFront.collider.CompareTag("Player")))
+            {
+                if (platformAhead.collider)
+                {
+                    jumpTarget = platformAhead.point;
+                    calculatedJumpVelocity = CalculateJumpVelocity(jumpTarget);
+                    shouldJump = true;
+                }
+            }
+            else if (isPlayerAbove && platformAbove.collider)
+            {
+                jumpTarget = platformAbove.point;
+                calculatedJumpVelocity = CalculateJumpVelocity(jumpTarget);
+                shouldJump = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = GroundCheck();
+        if (isGrounded && shouldJump)
+        {
+            shouldJump = false;
+            rb.AddForce(calculatedJumpVelocity, ForceMode2D.Impulse);
+        }
+    }
+
+    // Calculate the required jump velocity to reach the target point
+    private Vector2 CalculateJumpVelocity(Vector2 target)
+    {
+        float gravity = Mathf.Abs(Physics2D.gravity.y * rb.gravityScale);
+        Vector2 start = rb.position;
+        float dx = target.x - start.x;
+        float dy = target.y - start.y;
+        float h = jumpArcHeight;
+
+        // Calculate time to reach the apex
+        float vy = Mathf.Sqrt(2 * gravity * h);
+        float t_up = vy / gravity;
+        float t_down = Mathf.Sqrt(2 * (h - dy) / gravity);
+        float t_total = t_up + t_down;
+        float vx = dx / t_total;
+
+        return new Vector2(vx, vy);
+    }
+}
